46e8cf8b65b15fba38c1010c81d68802f175d4e8
Michel Carroll <carrmi.0@gmail.com>
2023-03-18 19:44:55 -0400

    Initial

--- src/lib.rs
+++ src/lib.rs
@@ -1,125 +1,132 @@
+use std::{sync::mpsc::{self, Receiver}, panic, borrow::BorrowMut};
 
-use std::f64;
+use specs::prelude::*;
 use wasm_bindgen::prelude::*;
+
+use web_sys::CanvasRenderingContext2d;
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(string: &str);
 }
 
-#[wasm_bindgen]
-pub fn start() {
+#[derive(Clone, Copy)]
+struct CanvasSize {
+    width: f64, height: f64
+}
+
+#[derive(Debug)]
+enum UserEvent {
+    Right, Left, Up, Down
+}
+
+struct CanvasHandle {
+    context: CanvasRenderingContext2d
+}
+
+fn create_canvas(size: CanvasSize) -> CanvasHandle {
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.create_element("canvas").unwrap();
+    canvas.set_attribute("width", &size.width.to_string()).unwrap();
+    canvas.set_attribute("height", &size.height.to_string()).unwrap();
+
     document.body().unwrap().append_child(&canvas).unwrap();
     
     let canvas: web_sys::HtmlCanvasElement = canvas
         .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
         .unwrap();
 
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
+    CanvasHandle {
+        context: canvas
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .unwrap()
+    }
+}
 
-    context.begin_path();
+#[derive(Default)]
+struct LastUserEvent {
+    event: Option<UserEvent>
+}
 
-    // Draw the outer circle.
-    context
-        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
+#[wasm_bindgen]
+pub fn start() {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
-
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    context.stroke();
-
-
-    // The `World` is our
-    // container for components
-    // and other resources.
+    let size = CanvasSize { width: 500.0, height: 500.0 };
+    let canvas_handle = create_canvas(size);
 
     let mut world = World::new();
-    world.register::<Pos>();
-    world.register::<Vel>();
+    world.register::<Position>();
+    world.insert(LastUserEvent::default());
 
-    // An entity may or may not contain some component.
+    world.create_entity().with(Position { x: 200.0, y: 100.0 }).build();
+    world.create_entity().with(Position { x: 100.0, y: 100.0 }).build();
+    world.create_entity().with(Position { x: 400.0, y: 100.0 }).build();
+    world.create_entity().with(Position { x: 300.0, y: 100.0 }).build();
 
-    world.create_entity().with(Vel(2.0)).with(Pos(0.0)).build();
-    world.create_entity().with(Vel(4.0)).with(Pos(1.6)).build();
-    world.create_entity().with(Vel(1.5)).with(Pos(5.4)).build();
+    let rendering = Rendering {
+        canvas_size: size,
+        rendering_context: canvas_handle.context
+    };
+    let mut dispatcher = DispatcherBuilder::new().with(rendering, "rendering", &[]).build();
 
-    // This entity does not have `Vel`, so it won't be dispatched.
-    world.create_entity().with(Pos(2.0)).build();
-
-    // This builds a dispatcher.
-    // The third parameter of `add` specifies
-    // logical dependencies on other systems.
-    // Since we only have one, we don't depend on anything.
-    // See the `full` example for dependencies.
-    let mut dispatcher = DispatcherBuilder::new().with(SysA, "sys_a", &[]).build();
-
-    // This dispatches all the systems in parallel (but blocking).
     dispatcher.dispatch(&mut world);
+    
+
+    let a = Closure::<dyn FnMut(_)>::new(move |e: web_sys::KeyboardEvent| {
+
+        let mut last_user_event = world.write_resource::<LastUserEvent>();
+        match e.key_code() {
+            37 => last_user_event.event = UserEvent::Left.into(),
+            38 => last_user_event.event = UserEvent::Up.into(),
+            39 => last_user_event.event = UserEvent::Right.into(),
+            40 => last_user_event.event = UserEvent::Down.into(),
+            _ => ()
+        };
+        drop(last_user_event);
+        
+        dispatcher.dispatch(&mut world);
+    });
+    web_sys::window().unwrap().add_event_listener_with_callback("keydown", a.as_ref().unchecked_ref()).unwrap();
+    a.forget();
+
+
+}
+ 
+
+
+struct Position { 
+    x: f64,
+    y: f64,
 }
 
-
-
-extern crate specs;
-
-use specs::prelude::*;
-
-// A component contains data which is
-// associated with an entity.
-
-struct Vel(f32);
-
-impl Component for Vel {
+impl Component for Position {
     type Storage = VecStorage<Self>;
 }
 
-struct Pos(f32);
-
-impl Component for Pos {
-    type Storage = VecStorage<Self>;
+struct Rendering {
+    canvas_size: CanvasSize,
+    rendering_context: CanvasRenderingContext2d
 }
 
-struct SysA;
+// Safe because wasm is always strictly single-threaded
+unsafe impl Send for Rendering {}
 
-impl<'a> System<'a> for SysA {
-    // These are the resources required for execution.
-    // You can also define a struct and `#[derive(SystemData)]`,
-    // see the `full` example.
-    type SystemData = (WriteStorage<'a, Pos>, ReadStorage<'a, Vel>);
-
-    fn run(&mut self, (mut pos, vel): Self::SystemData) {
-        // The `.join()` combines multiple components,
-        // so we only access those entities which have
-        // both of them.
-
-        // This joins the component storages for Position
-        // and Velocity together; it's also possible to do this
-        // in parallel using rayon's `ParallelIterator`s.
-        // See `ParJoin` for more.
-        for (pos, vel) in (&mut pos, &vel).join() {
-            alert("Hey");
-            pos.0 += vel.0;
+impl<'a> System<'a> for Rendering {
+    
+    type SystemData = (ReadStorage<'a, Position>, Write<'a, LastUserEvent>);
+    
+    fn run(&mut self, (pos, mut last_user_event): Self::SystemData) {
+        self.rendering_context.set_fill_style(&"#556688".into());
+        self.rendering_context.fill_rect(0.0, 0.0,self.canvas_size.width, self.canvas_size.height);
+        alert(&format!("{:?}", last_user_event.event));
+        last_user_event.event = None;
+        for pos in (&pos).join() {
+            self.rendering_context.set_fill_style(&"#885511".into());
+            self.rendering_context.fill_rect(pos.x, pos.y,50.0, 50.0);
         }
     }
 }

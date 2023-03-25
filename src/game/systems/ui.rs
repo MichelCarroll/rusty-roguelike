d685a0abb3c6fd154dc3cde9d8e3854e903356b7
Michel Carroll <carrmi.0@gmail.com>
2023-03-25 19:07:57 -0400

    Stuff

--- src/game/systems/ui.rs
+++ src/game/systems/ui.rs
@@ -4,7 +4,7 @@ use specs::prelude::*;
 
 use crate::game::{
     components::{
-        damageable::Damageable, describable::Describable, player_controlled::PlayerControlled,
+        damageable::Damageable, describable::Describable, player_controlled::PlayerControlled, sighted::Sighted,
     },
     ui::game_ui::GameUI,
     world::{UIState, WorldPosition, WorldPositionLookupTable},
@@ -20,13 +20,14 @@ impl<'a> System<'a> for UI {
         ReadStorage<'a, PlayerControlled>,
         ReadStorage<'a, Damageable>,
         ReadStorage<'a, Describable>,
+        ReadStorage<'a, Sighted>,
         Read<'a, UIState>,
         Read<'a, WorldPositionLookupTable>,
     );
 
     fn run(
         &mut self,
-        (player_controlled, damageable, describable, ui_state, world_position_lookup_table): Self::SystemData,
+        (player_controlled, damageable, describable, sighted, ui_state, world_position_lookup_table): Self::SystemData,
     ) {
         for (_, damageable) in (&player_controlled, &damageable).join() {
             self.ui_state.player_health.set(damageable.health)
@@ -35,20 +36,28 @@ impl<'a> System<'a> for UI {
         if mouse_position == self.last_mouse_over_position {
             return;
         }
+
         let mut lock = self.ui_state.inspected_entities.lock_mut();
         lock.clear();
-        if let Some(mouse_position) = mouse_position {
-            if let Some(entities) = world_position_lookup_table
-                .world_position_entities
-                .get(&mouse_position)
-            {
-                for entity in entities {
-                    if let Some(description) = describable.get(*entity) {
-                        lock.push_cloned(description.description.clone());
+
+        for (_, sighted) in (&player_controlled, &sighted).join() {
+            if let Some(mouse_position) = mouse_position {
+                if let Some(entities) = world_position_lookup_table
+                    .world_position_entities
+                    .get(&mouse_position)
+                {
+                    for entity in entities {
+                        if !sighted.seen.contains(entity.id()) && !sighted.seen_recently.contains(entity.id()) {
+                            continue;
+                        }
+                        if let Some(description) = describable.get(*entity) {
+                            lock.push_cloned(description.description.clone());
+                        }
                     }
                 }
+                self.last_mouse_over_position = mouse_position.into();
             }
-            self.last_mouse_over_position = mouse_position.into();
         }
+        
     }
 }
